58d76e8bbd580eb80c27e7a5541e317bfee2aaf5
saliou.conde <saliou.conde@iem.de>
2021-04-17 21:13:11 +0200

    App Engine.

--- src/main/java/de/iem/myfirstdocker/controller/MyFirstDockerApplicationController.java
+++ src/main/java/de/iem/myfirstdocker/controller/MyFirstDockerApplicationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/docker")
 public class MyFirstDockerApplicationController {
 
-    @GetMapping("hello")
+    @GetMapping("/")
     public ResponseEntity<String> helloWorld(){
 
         return new ResponseEntity<>("Hello World, " +
